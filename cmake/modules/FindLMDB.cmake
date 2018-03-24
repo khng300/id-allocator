90971a36f6f7a6f5e1929af2bea721beed6efc5a
Ka Ho Ng <khng300@gmail.com>
2018-03-24 14:45:02 +0800

    Use FindLMDB.cmake from https://github.com/apache/incubator-singa

--- cmake/modules/FindLMDB.cmake
+++ cmake/modules/FindLMDB.cmake
@@ -1,9 +1,30 @@
-# - Try to find LMDB
-# Once done this will define
-#  LMDB_FOUND - System has LMDB
-#  LMDB_INCLUDE_DIRS - The LMDB include directories
-#  LMDB_LIBRARIES - The libraries needed to use LMDB
-#  LMDB_LIBRARY_DIRS - The directories containing LMDB library
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 
-find_package (PkgConfig REQUIRED)
-pkg_search_module (LMDB REQUIRED lmdb)
+
+FIND_PATH(LMDB_INCLUDE_DIR NAMES lmdb.h PATHS "$ENV{LMDB_DIR}/include")
+FIND_LIBRARY(LMDB_LIBRARIES NAMES lmdb PATHS "$ENV{LMDB_DIR}/include")
+
+INCLUDE(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LMDB DEFAULT_MSG LMDB_INCLUDE_DIR LMDB_LIBRARIES)
+
+IF(LMDB_FOUND)
+    MESSAGE(STATUS "Found lmdb at ${LMDB_INCLUDE_DIR}")
+    MARK_AS_ADVANCED(LMDB_INCLUDE_DIR LMDB_LIBRARIES)
+    
+ENDIF()
